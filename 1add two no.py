2c019fa3ae5e1babf8bae2311686f914426c473e
Akhtar ALI <165200174+AkhtarAli77@users.noreply.github.com>
2024-05-19 16:33:13 +0530

    python code

---
+++ 1add two no.py
@@ -0,0 +1,4 @@
+num1 = int(input("enter a number here:"))
+num2 = int(input("enter a number here:"))
+sum = num1+num2
+print("the sum of given two number is ", sum)
---
+++ 3square root.py
@@ -0,0 +1,4 @@
+num = 64
+# num1 = int(input("enter a number here : "))
+sr = num**(1/2)
+print("The square root fo the given number is",sr )
